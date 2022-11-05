fb994eb2f2f7058c0b84dcc05fc557304ab83bed
ramyaramana <77260289+ramyaramana@users.noreply.github.com>
2022-11-05 15:39:26 +0530

    Update deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -1,2 +1,2 @@
 ps | grep java-fullstack | awk '{print $1}' | xargs kill -9 || true
-env SERVER.PORT=8081 nohup java -jar ./target/java-fullstack-1.0-SNAPSHOT.jar &
+env SERVER.PORT=8080 nohup java -jar ./target/java-fullstack-1.0-SNAPSHOT.jar &
